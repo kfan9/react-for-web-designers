3707a6cd45acf238f942b7c7b6558ef59cd0e420
Kang Jye Fan <kfan9@uic.edu>
2018-05-04 21:30:53 +0000

    worked through 8 - color selector

--- React/Ex_Files_React_Designers/Exercise Files/Ch02/02_01/start/app.js
+++ React/Ex_Files_React_Designers/Exercise Files/Ch02/02_01/start/app.js
@@ -1,45 +1,84 @@
 (function() {
   "use strict";
 
-  function SizeSelector(props){
-    
-    function sizeOpetions(){
-      var sizes = window.Inventory.allSizes;
-      
-      return sizes.map(function(num){
-          return(
-        <option value={num} key={num}>{num}</option>
+
+  // for Size
+  function SizeSelector(props) {
+    function sizeOptions() {
+      return props.sizes.map(function(num) {
+        return (
+          <option value={num} key={num}>
+            {num}
+          </option>
         );
       });
     }
-    
-    
-    return(
-      	<div className="field-group">
-								<label htmlFor="size-options">Size:</label>
-								<select defaultValue={props.size} name="sizeOptions" id="size-options">
-                {sizeOpetions()}
-								</select>
-							</div>
-      );
+
+    function onSizeChange(evt) {
+      console.log("event change fired: ", evt.target.value);
+      props.handleSizeChange(evt.target.value);
+    }
+
+    return (
+      <div className="field-group">
+        <label htmlFor="size-options">Size:</label>
+        <select defaultValue={props.size} name="sizeOptions" id="size-options" onChange={onSizeChange}>
+          {sizeOptions()}
+        </select>
+      </div>
+    );
   }
 
+  // for Color
+  function ColorSelector(props) {
+    function colorOptions() {
+      return props.colors.map(function(name) {
+        return (
+          <option value={name} key={name}>
+            {name}
+          </option>
+        );
+      });
+    }
+
+    return (
+      <div className="field-group">
+        <label htmlFor="color-options">Color:</label>
+        <select defaultValue={props.color} name="colorOptions" id="color-options">
+          {colorOptions()}
+        </select>
+      </div>
+    );
+  }
+
+
+  // putting in images
   function ProductImage(props) {
     return <img src={`../../../assets/${props.color}.jpg`} alt="Product Image" />;
   }
   
   
-  
-  
+  // making a var for creating class
   var ProductCustomizer = createReactClass({
     getInitialState: function() {
-      var sizes = window.Inventory.allSizes;
+      var sizes = window.Inventory.allSizes,
+        colors = window.Inventory.allColors;
 
       return {
         color: "red",
+        colors: colors,
         size: 8,
         sizes: sizes
       };
+    },
+
+    handleSizeChange: function(selectedSize) {
+      console.log('parent handleSizeChange', selectedSize);
+      
+      var availColors = window.Inventory.bySize[selectedSize];
+      this.setState({
+        colors: availColors
+      });
     },
 
     render: function() {
@@ -49,7 +88,8 @@
             <ProductImage color={this.state.color} />
           </div>
           <div className="selectors">
-            <SizeSelector size={this.state.size} sizes={this.state.sizes} />
+            <SizeSelector size={this.state.size} sizes={this.state.sizes} handleSizeChange={this.handleSizeChange}/>
+            <ColorSelector color={this.state.color} colors={this.state.colors} />
           </div>
         </div>
       );
